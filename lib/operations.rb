4f35b8854532fbb931ab6bb1752ee2f5cabff98f
Joan Lyness <joanrigdon@gmail.com>
2019-02-21 06:39:46 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,11 @@
 def unsafe?(speed)
-	if speed < 40? true : false
-	if speed > 60? true : false
+	speed < 40? true : false
+	speed > 60? true : false
 end
 
 
 
 def not_safe?(speed)
-	if speed > 60? true : false
-	if speed < 40? true : false
+	speed > 60? true : false
+	speed < 40? true : false
 end
